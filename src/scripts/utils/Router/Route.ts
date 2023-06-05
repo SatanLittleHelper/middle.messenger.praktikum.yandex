db1916703c354ab1772427e244f19dc55cde95d4
Alexander <madhamst3r@gmail.com>
2023-06-06 01:19:40 +0300

    add Router.ts

---
+++ src/scripts/utils/Router/Route.ts
@@ -0,0 +1,49 @@
+import isEqual from "../isEqual";
+import Block from "../Block";
+import renderDOM from "../renderDOM";
+
+export interface RouteProps {
+    querySelector: string;
+}
+export class Route {
+    private _pathname: string;
+    private readonly _blockClass: any;
+    private _block: Block | null;
+    private _props: Record<string, any>;
+
+    constructor(pathname:string, view:Block, props:RouteProps) {
+        this._pathname = pathname;
+        this._blockClass = view;
+        this._block = null;
+        this._props = props;
+    }
+
+    navigate(pathname) {
+        if (this.match(pathname)) {
+            this._pathname = pathname;
+            this.render();
+        }
+    }
+
+    leave() {
+        if (this._block) {
+            this._block.hide();
+        }
+    }
+
+    match(pathname) {
+        return isEqual(pathname, this._pathname);
+    }
+
+    render() {
+        if (!this._block) {
+            this._block = new this._blockClass();
+            renderDOM(<Block>this._block, this._props.querySelector);
+            return;
+        }
+
+        this._block.show();
+    }
+}
+
+
---
+++ src/scripts/utils/Router/Router.ts
@@ -0,0 +1,68 @@
+import {Route} from "./Route";
+
+export class Router {
+    private static __instance: Router;
+    private routes: Route[];
+    private history: History;
+    private _currentRoute: Route | null;
+    private readonly _rootQuery: string;
+    constructor(rootQuery) {
+        if (Router.__instance) {
+            return Router.__instance;
+        }
+
+        this.routes = [];
+        this.history = window.history;
+        this._currentRoute = null;
+        this._rootQuery = rootQuery;
+
+        Router.__instance = this;
+    }
+
+    use(pathname, block) {
+        const route = new Route(pathname, block, {querySelector: this._rootQuery});
+
+        this.routes.push(route);
+
+        return this;
+    }
+
+    start() {
+        window.onpopstate = (event => {
+            this._onRoute(event.currentTarget.location.pathname);
+        }).bind(this);
+
+        this._onRoute(window.location.pathname);
+    }
+
+    _onRoute(pathname) {
+        const route = this.getRoute(pathname);
+        if (!route) {
+            return;
+        }
+
+        if (this._currentRoute && this._currentRoute !== route) {
+            this._currentRoute.leave();
+        }
+
+        this._currentRoute = route;
+        route.render();
+    }
+
+    go(pathname) {
+        this.history.pushState({}, '', pathname);
+        this._onRoute(pathname);
+    }
+
+    back() {
+        this.history.back();
+    }
+
+    forward() {
+        this.history.forward();
+    }
+
+    getRoute(pathname) {
+        return this.routes.find(route => route.match(pathname));
+    }
+}
